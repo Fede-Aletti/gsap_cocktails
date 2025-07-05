3320047844f6323f6efe4b1a05fd531dcb27a79b
Fede Aletti <fedaletti@gmail.com>
2025-07-05 14:10:34 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import gsap from "gsap";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="flex-center h-screen">
+      <h1 className="text-3xl text-indigo-300">Hello GSAP</h1>
+    </div>
+  );
+};
+
+export default App;
